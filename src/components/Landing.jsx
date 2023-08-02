2e7029c02d0c220e7227bd16c89b5bb7f68a226f
Zak Franczak <zakfranczak@gmail.com>
2023-08-02 10:23:50 -0500

    added api key

---
+++ src/components/Landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Watch Wise</h1>
+      <h2>The Philosopher of Movie Night</h2>
+      <h3>
+        Embrace movie serenity with WatchWise as your spiritual companion in
+        managing and discovering films.
+      </h3>
+    </div>
+  );
+};
+
+export default Landing;
