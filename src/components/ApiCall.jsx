b071d2d9f27126b8700064b2ced7ba4cdf59f17c
Zak Franczak <zakfranczak@gmail.com>
2023-08-04 10:15:51 -0500

    trying to add providers, not having success yet.

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -6,6 +6,8 @@ const token = import.meta.env.VITE_TMDB_TOKEN;
 
 const ApiCall = () => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [providers, setProviders] = useState([]);
 
   const options = {
     method: 'GET',
@@ -16,6 +18,7 @@ const ApiCall = () => {
   };
 
   const fetchMovies = () => {
+    // Fetch trending movies
     fetch(
       'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
       options
@@ -25,9 +28,34 @@ const ApiCall = () => {
       .catch((err) => console.error(err));
   };
 
+  const fetchProviders = (movieId) => {
+    // Fetch movie providers for the selected movie
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/watch/providers`,
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const usProviders = data.results.US; // Get providers for the US
+
+        if (usProviders && usProviders.link) {
+          const providerLogos = usProviders.flatrate.map((provider) => ({
+            logo: `https://image.tmdb.org/t/p/original${provider.logo_path}`,
+            name: provider.provider_name,
+          }));
+
+          setProviders(providerLogos);
+        } else {
+          setProviders([]);
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div>
       <button onClick={fetchMovies}>See Today's Trending Movies</button>
+
       <div className='movie-block'>
         {movies.map((movie) => (
           <div key={movie.id} className='single-movie'>
@@ -37,7 +65,42 @@ const ApiCall = () => {
               className='movie-poster'
             />
             <h2 className='movie-title'>{movie.title}</h2>
-            {/* <p className='movie-overview'>{movie.overview}</p> */}
+
+            {/* Button to fetch and display movie providers */}
+            <button
+              onClick={() => {
+                setSelectedMovie(movie);
+                fetchProviders(movie.id);
+              }}
+            >
+              Get Providers
+            </button>
+
+            {/* Display providers for the selected movie */}
+            {providers.map((provider, index) => (
+              <div key={index} className='provider'>
+                <img
+                  src={provider.logo}
+                  alt={provider.name}
+                  className='provider-logo'
+                />
+                <p className='provider-name'>{provider.name}</p>
+              </div>
+            ))}
+
+            {/* Debugging */}
+            {console.log(movie)}
+            {console.log('Provider : ')}
+            {providers.map((provider) => console.log(provider.name))}
+
+            {/* Additional provider block (commented out) */}
+            {/* <div className='provider-block'>
+                {providers.map((provider) => (
+                    <div key='/' className='provider'>
+                        <p className='provider-name'>{provider.}</p>
+                    </div>
+                ))}
+            </div> */}
           </div>
         ))}
       </div>
