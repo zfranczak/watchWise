7573df8670eee5a43c03655b9d6f96538fdb2e1f
Zak Franczak <zakfranczak@gmail.com>
2023-08-08 10:37:06 -0500

    implementing react-router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Landing from './components/Landing';
 import ApiCall from './components/ApiCall';
+import WatchList from './components/WatchList';
+import Watched from './components/Watched';
+import Add from './components/Add';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navigation />
-      <Landing />
-      <ApiCall />
+      <Router>
+        <Navigation />
+
+        <Switch>
+          <Route exact path='/'>
+            <Landing />
+            <ApiCall />
+          </Route>
+
+          <Route path='/watched'>
+            <Watched />
+          </Route>
+
+          <Route path='/add'>
+            <Add />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
