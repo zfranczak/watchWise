01e4bd4fc91619e353011ce2895d4a5ac56ee383
Zak Franczak <zakfranczak@gmail.com>
2023-08-12 09:37:34 -0500

    Added movie modal for more info.  Provider moved to modal

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/api-call.css';
+import Modal from '../modals/Modal';
 
 const apiKey = import.meta.env.VITE_TMDB_KEY;
 const token = import.meta.env.VITE_TMDB_TOKEN;
 
 const ApiCall = () => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   const options = {
     method: 'GET',
@@ -67,11 +69,23 @@ const ApiCall = () => {
     fetchMovies(); // Fetch movies and providers on component mount
   }, []);
 
+  const openMovieDetails = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const closeMovieDetails = () => {
+    setSelectedMovie(null);
+  };
+
   return (
     <div>
       <div className='movie-block'>
         {movies.map((movie) => (
-          <div key={movie.id} className='single-movie'>
+          <div
+            key={movie.id}
+            className='single-movie'
+            onClick={() => openMovieDetails(movie)}
+          >
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
@@ -79,22 +93,18 @@ const ApiCall = () => {
             />
             <h2 className='movie-title'>{movie.title}</h2>
 
-            <div className='provider-container'>
-              {movie.providers &&
-                movie.providers.map((provider, index) => (
-                  <div key={index} className='provider'>
-                    <img
-                      src={provider.logo}
-                      alt={provider.name}
-                      className='provider-logo'
-                    />
-                    {/* <p className='provider-name'>{provider.name}</p> */}
-                  </div>
-                ))}
-            </div>
+            {console.log(movie)}
           </div>
         ))}
+        {selectedMovie && (
+          <Modal
+            isOpen={selectedMovie !== null}
+            onClose={closeMovieDetails}
+            movie={selectedMovie}
+          />
+        )}
       </div>
+
       <p className='attribute'>Provider Data supplied by JustWatch</p>
     </div>
   );
