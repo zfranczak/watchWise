d9022906e4354fd5f6112e07fb60ce1317456da1
Zak Franczak <zakfranczak@gmail.com>
2023-10-11 15:12:45 -0500

    Added local storage but broke the 'watched' list

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,10 +1,11 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { AppReducer } from './AppReducer';
 
 // Initial state
 const initialState = {
-  watchlist: [],
-  watched: [],
+  watchlist: localStorage.getItem('watchlist')
+    ? JSON.parse(localStorage.getItem('watchlist'))
+    : [],
 };
 
 // Create context
@@ -13,6 +14,10 @@ export const GlobalContext = createContext(initialState);
 // Provider component
 export const GlobalProvider = (props) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+  }, [state]);
 
   // Actions
   const addMovieToWatchlist = (movie) => {
